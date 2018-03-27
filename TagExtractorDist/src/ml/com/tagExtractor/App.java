9fd287cd37235a7c8e54c841a16a85523985e18a
git test <git_test@mail.com>
2018-03-27 18:58:59 +0900

    mod

--- TagExtractorDist/src/ml/com/tagExtractor/App.java
+++ TagExtractorDist/src/ml/com/tagExtractor/App.java
@@ -3,24 +3,15 @@ package ml.com.tagExtractor;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
     	
     	TagExtractor te = new TagExtractor();
-    	try {
-			List<String> s = te.extractTagFromWebPage("https://redis.io/topics/quickstart");
-			for(String ss : s ){
-				System.out.println(ss);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		List<String> tags = te.extractTagFromWebPage("https://en.wikipedia.org/wiki/Spring_Framework"); // extract tags from web page
+
+		for(String tag : tags ){
+			System.out.println(tag); // print extracted tags
 		}
-    	
 
     }
 }
